71248ff754957648a2dbcabb237ff23a2e5ed88c
xulaing <khauv@live.fr>
2022-11-16 15:09:16 +0100

    oui

--- bot.py
+++ bot.py
@@ -1,20 +1,45 @@
 import discord
 import responses
-
+import random
 
 # Send messages
-async def send_message(message, user_message, is_private):
+async def send_message(user_id, message, user_message, is_private):
     try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        p_message = user_message.lower()
+        if p_message == 'hello':
+            response = 'Coucou !'
 
+        elif p_message == 'roll':
+            response = str(random.randint(1, 6))
+
+        elif p_message == '!help':
+            response = "`This is a help message that you can modify.`"
+    
+        elif p_message == 'qui a raison ?' or p_message=='qui est la plus belle ?':
+            cici = '<@588096973446316038>'
+            response = 'c\'est %s.' % cici
+
+        elif p_message == 'qui je suis ?':
+            if int(user_id) == 588096973446316038 :
+                response = 'La petite princesse de matmat trop belle qui pipoupi la vie'
+            elif int(user_id) == 184405311681986560 :
+                response = 'Matimatou, le petit cochonou que cici aime troooooop !!'
+    
+        elif p_message == 'send bobi':
+            index = random.randint(1, 6)
+            bark = ["bawrf", "bwirf", 'gnrrr', "bouf"]
+            bark_index = random.randint(0, 3)
+            await message.author.send(file=discord.File('./bobi/bobi_%d.jpg' % index)) if is_private else await message.channel.send(file=discord.File('./bobi/bobi_%d.jpg' % index))
+            response = bark[bark_index]
+
+        await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
 
 
 def run_discord_bot():
-    TOKEN = 'YOUR_KEY'
-    client = discord.Client()
+    TOKEN = 'MTA0MjM4MzA2MDQyMDgwMDU2Mg.GywUgF.1qno6P0_sUUJJOLCEQskrsfJeVTzYoy5xILzEo'
+    client = discord.Client(intents=discord.Intents.all())
 
     @client.event
     async def on_ready():
@@ -28,8 +53,15 @@ def run_discord_bot():
 
         # Get data about the user
         username = str(message.author)
+        user_id = str(message.author.id)
         user_message = str(message.content)
         channel = str(message.channel)
+
+        members = client.guilds[0].members
+
+        member_ids = [member.id for member in members if not member.bot]
+
+        print(member_ids)
 
         # Debug printing
         print(f"{username} said: '{user_message}' ({channel})")
@@ -37,9 +69,9 @@ def run_discord_bot():
         # If the user message contains a '?' in front of the text, it becomes a private message
         if user_message[0] == '?':
             user_message = user_message[1:]  # [1:] Removes the '?'
-            await send_message(message, user_message, is_private=True)
+            await send_message(user_id, message, user_message, is_private=True)
         else:
-            await send_message(message, user_message, is_private=False)
+            await send_message(user_id, message, user_message, is_private=False)
 
     # Remember to run your bot with your personal TOKEN
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,15 +1,28 @@
 import random
 
 
-def handle_response(message) -> str:
+def handle_response(message, user_id) -> str:
     p_message = message.lower()
     if p_message == 'hello':
-        return 'Hey there!'
+        return 'Coucou !'
 
     if p_message == 'roll':
         return str(random.randint(1, 6))
 
     if p_message == '!help':
         return "`This is a help message that you can modify.`"
+    
+    if p_message == 'qui a raison ?' or p_message=='qui est la plus belle ?':
+        cici = '<@588096973446316038>'
+        return 'c\'est %s.' % cici
 
-    #  return 'Yeah, I don\'t know. Try typing "!help".'
+    if p_message == 'qui je suis ?':
+        if int(user_id) == 588096973446316038 :
+            print('passed')
+            return 'La petite princesse de matmat trop belle qui pipoupi la vie'
+        elif int(user_id) == 184405311681986560 :
+            return 'Matimatou, le petit cochonou que cici aime troooooop !!'
+    
+    if p_message == 'send bobi':
+        index = random.randint(1, 6)
+        return "file=discord.File('./bobi/bobi_%d.jpg')" % index
